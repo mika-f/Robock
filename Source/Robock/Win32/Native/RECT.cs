598c65112de2b368d50bfd613d0c2cd7da023db3
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-07-21 02:05:01 +0900

    fix: add item

---
+++ Source/Robock/Win32/Native/RECT.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable  InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace Robock.Win32.Native
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+}
