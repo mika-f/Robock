9ae7c96592bfcd499da41860dc333d99ea1eccb0
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-07-24 21:27:59 +0900

    feat: Don't display some windows.
    Can't render these windows by DWM.

---
+++ Source/Robock/Models/Ignores.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Robock.Models
+{
+    public class Ignores
+    {
+        public static List<string> IgnoreWindowTitles = new List<string>
+        {
+            "ChromeWindow", // Robock's window separator
+            "Robock", // Myself
+            "Robock.Background", // Myself
+            "NVIDIA GeForce Overlay", //
+            "Program Manager" // Desktop
+        };
+    }
+}
