19d38812e5580914625d022dc7805d7297838073
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-07-25 23:04:05 +0900

    feat(shared): Dynamic Import

--- Source/Robock.Shared/Win32/NativeMethods.cs
+++ Source/Robock.Shared/Win32/NativeMethods.cs
@@ -12,6 +12,12 @@ namespace Robock.Shared.Win32
         [DllImport("kernel32.dll")]
         public static extern uint GetLastError();
 
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
         #endregion
 
         #region Gdi32
