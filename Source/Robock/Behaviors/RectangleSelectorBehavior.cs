60b937e7f87e4f8d31ff3520858a4901008440c2
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-08-02 08:34:28 +0900

    feat(core): Save/Restore select area

--- Source/Robock/Behaviors/RectangleSelectorBehavior.cs
+++ Source/Robock/Behaviors/RectangleSelectorBehavior.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interactivity;
 using System.Windows.Shapes;
@@ -8,24 +11,27 @@ namespace Robock.Behaviors
     public class RectangleSelectorBehavior : Behavior<FrameworkElement>
     {
         public static readonly DependencyProperty LeftProperty =
-            DependencyProperty.Register(nameof(Left), typeof(int), typeof(RectangleSelectorBehavior));
+            DependencyProperty.Register(nameof(Left), typeof(int), typeof(RectangleSelectorBehavior), new FrameworkPropertyMetadata(LeftPropertyChanged));
 
         public static readonly DependencyProperty TopProperty =
-            DependencyProperty.Register(nameof(Top), typeof(int), typeof(RectangleSelectorBehavior));
+            DependencyProperty.Register(nameof(Top), typeof(int), typeof(RectangleSelectorBehavior), new FrameworkPropertyMetadata(TopPropertyChanged));
 
         public static readonly DependencyProperty HeightProperty =
-            DependencyProperty.Register(nameof(Height), typeof(int), typeof(RectangleSelectorBehavior));
+            DependencyProperty.Register(nameof(Height), typeof(int), typeof(RectangleSelectorBehavior), new FrameworkPropertyMetadata(HeightPropertyChanged));
 
         public static readonly DependencyProperty WidthProperty =
-            DependencyProperty.Register(nameof(Width), typeof(int), typeof(RectangleSelectorBehavior));
+            DependencyProperty.Register(nameof(Width), typeof(int), typeof(RectangleSelectorBehavior), new FrameworkPropertyMetadata(WidthPropertyChanged));
 
         public static readonly DependencyProperty RectangleProperty =
             DependencyProperty.Register(nameof(Rectangle), typeof(Rectangle), typeof(RectangleSelectorBehavior));
+
+        private IDisposable _disposable;
 
         private double _fromPosX;
         private double _fromPosY;
 
         private bool _isSelecting;
+        private Subject<int> _subject;
 
         public int Left
         {
@@ -57,12 +63,70 @@ namespace Robock.Behaviors
             set => SetValue(RectangleProperty, value);
         }
 
+        private static void LeftPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is RectangleSelectorBehavior behavior))
+                return;
+            behavior._subject.OnNext((int) e.NewValue);
+        }
+
+        private static void TopPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is RectangleSelectorBehavior behavior))
+                return;
+            behavior._subject.OnNext((int) e.NewValue);
+        }
+
+        private static void HeightPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is RectangleSelectorBehavior behavior))
+                return;
+            behavior.Rectangle.Height = (int) e.NewValue;
+            behavior.UpdateRenderStatus();
+        }
+
+        private static void WidthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is RectangleSelectorBehavior behavior))
+                return;
+            behavior.Rectangle.Width = (int) e.NewValue;
+            behavior.UpdateRenderStatus();
+        }
+
         protected override void OnAttached()
         {
             base.OnAttached();
+            _subject = new Subject<int>();
+            _disposable = _subject.Throttle(TimeSpan.FromMilliseconds(100)).Subscribe(_ =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Rectangle.Margin = new Thickness(Left, Top, 0, 0);
+                    UpdateRenderStatus();
+                });
+            });
+            AssociatedObject.DataContextChanged += AssociatedObjectOnDataContextChanged;
             AssociatedObject.MouseLeftButtonDown += AssociatedObjectOnMouseLeftButtonDown;
             AssociatedObject.MouseLeftButtonUp += AssociatedObjectOnMouseLeftButtonUp;
             AssociatedObject.MouseMove += AssociatedObjectOnMouseMove;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseMove -= AssociatedObjectOnMouseMove;
+            AssociatedObject.MouseLeftButtonUp -= AssociatedObjectOnMouseLeftButtonUp;
+            AssociatedObject.MouseLeftButtonDown -= AssociatedObjectOnMouseLeftButtonDown;
+            AssociatedObject.DataContextChanged -= AssociatedObjectOnDataContextChanged;
+            _subject.Dispose();
+            _disposable.Dispose();
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Rectangle == null)
+                return;
+            UpdateRenderStatus();
         }
 
         private void AssociatedObjectOnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -117,6 +181,16 @@ namespace Robock.Behaviors
                 Rectangle.Width = _fromPosX - pos.X;
                 Rectangle.Height = _fromPosY - pos.Y;
             }
+        }
+
+        private bool IsSelected()
+        {
+            return Math.Abs(Rectangle.Width) > 0 && Math.Abs(Rectangle.Height) > 0;
+        }
+
+        private void UpdateRenderStatus()
+        {
+            Rectangle.Visibility = IsSelected() ? Visibility.Visible : Visibility.Hidden;
         }
     }
 }
