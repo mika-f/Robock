f5ba2f90f45e272eafbd7c8cdd12c3124a49a35f
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-01-02 21:25:26 +0900

    feat: support hidpi

--- Source/Robock/ViewModels/VirtualScreenViewModel.cs
+++ Source/Robock/ViewModels/VirtualScreenViewModel.cs
@@ -4,7 +4,10 @@ using System.Reactive.Linq;
 using System.Windows;
 
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 
+using Robock.Services;
+using Robock.Shared.Extensions;
 using Robock.Shared.Mvvm;
 using Robock.ViewModels.Tabs;
 
@@ -17,8 +20,8 @@ namespace Robock.ViewModels
         public ObservableCollection<DesktopViewModel> Desktops { get; }
         public ReactiveProperty<int> SelectedIndex { get; }
 
-        public double VirtualScreenWidth => SystemParameters.VirtualScreenWidth / Scale;
-        public double VirtualScreenHeight => SystemParameters.VirtualScreenHeight / Scale;
+        public ReactiveProperty<double> VirtualScreenWidth { get; }
+        public ReactiveProperty<double> VirtualScreenHeight { get; }
 
         public VirtualScreenViewModel()
         {
@@ -31,7 +34,14 @@ namespace Robock.ViewModels
                 if (w >= Desktops.Count)
                     return;
                 Desktops[w].IsSelected.Value = true;
-            });
+            }).AddTo(this);
+            VirtualScreenWidth = new ReactiveProperty<double>(SystemParameters.VirtualScreenWidth / Scale * DpiService.Instance.CurrentDpi.ScaleY);
+            VirtualScreenHeight = new ReactiveProperty<double>(SystemParameters.VirtualScreenHeight / Scale * DpiService.Instance.CurrentDpi.ScaleX);
+            DpiService.Instance.ObserveProperty(w => w.CurrentDpi).Subscribe(w =>
+            {
+                VirtualScreenWidth.Value = SystemParameters.VirtualScreenWidth / Scale * DpiService.Instance.CurrentDpi.ScaleY;
+                VirtualScreenHeight.Value = SystemParameters.VirtualScreenHeight / Scale * DpiService.Instance.CurrentDpi.ScaleX;
+            }).AddTo(this);
         }
     }
 }
