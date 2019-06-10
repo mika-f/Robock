f5ea26dfad5cafedbbfcb6996705f231520b8ad2
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-06-10 22:35:25 +0900

    feat(robock): when visibility changed, reset position

---
+++ Source/Robock/Behaviors/ResetCanvasPositionOnVisibilityChangedBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Microsoft.Xaml.Behaviors;
+
+namespace Robock.Behaviors
+{
+    public class ResetCanvasPositionOnVisibilityChangedBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObjectOnIsVisibleChanged;
+        }
+
+        private void AssociatedObjectOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Canvas.SetLeft(AssociatedObject, 0);
+            Canvas.SetTop(AssociatedObject, 0);
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= AssociatedObjectOnIsVisibleChanged;
+            base.OnDetaching();
+        }
+    }
+}
