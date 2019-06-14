4eba1571c45a2b62ff97c077972241ae2c958430
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-06-14 22:16:35 +0900

    feat(interop): add UWP interop

---
+++ Source/Robock.Interop/Com/IDirect3DDxgiInterfaceAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Robock.Interop.Com
+{
+    [ComImport]
+    [Guid("A9B3D012-3DF2-4EE3-B8D1-8695F457D3C1")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IDirect3DDxgiInterfaceAccess : IDisposable
+    {
+        IntPtr GetInterface([In] ref Guid iid);
+    }
+}
