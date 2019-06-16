ae8bc2b9ad84dbdae3cfdffae58348cf954afb63
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-06-16 22:46:00 +0900

    fix(robock): buildable

---
+++ Source/Robock/Converters/AbsoluteToRelativeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Robock.Converters
+{
+    /// <summary>
+    ///     Absolute Value to Relative Value Converter
+    /// </summary>
+    internal class AbsoluteToRelativeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var value = double.Parse(values[0]?.ToString() ?? throw new InvalidOperationException());
+            var scale = double.Parse(values[1]?.ToString() ?? throw new InvalidOperationException());
+
+            return value / scale;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
